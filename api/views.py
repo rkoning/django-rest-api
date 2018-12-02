e29b3d2c59889b9483dcd9188a6f9bc3147097fd
Ryan Koning <rkoning8@gmail.com>
2018-12-02 12:19:20 -0700

    Added POST to campaigns

--- api/views.py
+++ api/views.py
@@ -5,17 +5,24 @@ from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Advertisement, Account, Campaign
 from .serializers import AccountSerializer, CampaignSerializer, AdvertisementSerializer
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def account_collection(request):
     if request.method == 'GET':
         accounts = Account.objects.all()
         serializer = AccountSerializer(accounts, many = True)
         return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def account_element(request, pk):
@@ -28,15 +35,22 @@ def account_element(request, pk):
         serializer = AccountSerializer(account)
         return Response(serializer.data)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def campaign_collection(request):
     if request.method == 'GET':
         limit = int(request.GET.get('limit', 100))
         offset = int(request.GET.get('offset', 0))
         campaigns = Campaign.objects.all()[offset:limit]
         # serializer = CampaignSerializer(campaigns, many = True)
+        # return Response(serializer.data)
         response = serializers.serialize("json", campaigns)
-        return HttpResponse(response, content_type = "application/json")
+        return JsonResponse(response, content_type = "application/json")
+    elif request.method == 'POST':
+        serializer = CampaignSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_405_BAD_REQUEST)
 
 @api_view(['GET'])
 def campaign_element(request, pk):
