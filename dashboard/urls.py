412c7543598cd2725e0a1f4b8eca71845aba7fa8
Ryan Koning <rkoning8@gmail.com>
2018-12-03 06:55:35 -0700

    Removed dashboard app

--- dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-from django.conf.urls import url
-
-urlpatterns = [
-    url(r'^', index, name = 'dashboard')
-]
--- dashboard/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from api.models import Account
-
-# Create your views here.
-def index(request):
-    account = Account.objects.all()[0]
-    campaigns = account.campaign_set.all()
-    return render(request, 'index.html', context = { 'account' : account, 'account_campaigns' : campaigns })
